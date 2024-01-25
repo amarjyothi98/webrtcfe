7a0bad9684080a1b7cc2fc0352b933f4a05666c8
amarjyothi98 <amarjyothi526@gmail.com>
2024-01-25 23:24:16 +0530

    error recorded | cors error solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Lobby from './screens/Lobby';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Lobby/>}/>
+      </Routes>
     </div>
   );
 }
--- src/screens/Lobby.jsx
+++ src/screens/Lobby.jsx
@@ -1,7 +1,27 @@
+import { useCallback, useState } from "react"
+
 export default function Lobby () {
+
+    const [email, setEmail] = useState("");
+    const [number, setNumber] = useState("");
+
+    const handleSubmitForm = useCallback((e) => {
+        e.preventDefault();
+        console.log({email, number});
+    }, [email, number])
+
     return(
         <>
-        
+        Lobby Screen
+        <form onSubmit={handleSubmitForm}>
+            <label htmlFor="email">Email</label>
+            <input type="Email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+            <br />
+            <label htmlFor="number">Room Number</label>
+            <input type="Number" id="number" onChange={(e) => setNumber(e.target.value)}/>
+            <br />
+            <button>Join</button>
+        </form>
         </>
     )
 }
